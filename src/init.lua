b41865bd8682ae0b328735f567e4ad377ad71745
TheNexusAvenger <zpcook@googlemail.com>
2020-11-24 16:52:23 -0500

    Set up project.

---
+++ src/init.lua
@@ -0,0 +1,9 @@
+--[[
+TheNexusAvenger
+
+Project for fetching resources of Nexus VR Core.
+--]]
+
+local NexusProject = require(script:WaitForChild("NexusProject"))
+
+return NexusProject.new(script)
