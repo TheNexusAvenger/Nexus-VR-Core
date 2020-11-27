15455e251d4e727f130e492a5a49ea4c02951223
TheNexusAvenger <zpcook@googlemail.com>
2020-11-26 22:50:53 -0500

    Fixed pressed inputs entering the frame registering as clicks.

--- src/VRSurfaceGui.lua
+++ src/VRSurfaceGui.lua
@@ -45,6 +45,8 @@ function VRSurfaceGui:__new(ExistingSurfaceGui)
     self.MouseDownFrames = {}
     self:DisableChangeReplication("ScrollLastPositions")
     self.ScrollLastPositions = {}
+    self:DisableChangeReplication("LastInputs")
+    self.LastInputs = {}
 end
 
 --[[
@@ -77,8 +79,9 @@ given relative points.
 function VRSurfaceGui:UpdateEvents(Points)
     --Determine the max trigger inputs.
     local MaxTriggerInputs = {}
+    local FrameInputId = {}
     local AbsoluteSize = self.AbsoluteSize
-    for _,Point in pairs(Points) do
+    for InputId,Point in pairs(Points) do
         local PosX,PosY,TriggerInput = Point.X * AbsoluteSize.X,Point.Y * AbsoluteSize.Y,Point.Z
         for _,Frame in pairs(self:GetDescendants()) do
             if Frame:IsA("GuiObject") then
@@ -86,6 +89,7 @@ function VRSurfaceGui:UpdateEvents(Points)
                 if FramePosition.X <= PosX and FramePosition.X + FrameSize.X >= PosX and FramePosition.Y <= PosY and FramePosition.Y + FrameSize.Y >= PosY then
                     if not MaxTriggerInputs[Frame] or TriggerInput > MaxTriggerInputs[Frame].Z then
                         MaxTriggerInputs[Frame] = Vector3.new(PosX,PosY,TriggerInput)
+                        FrameInputId[Frame] = InputId
                     end
                 end
             end
@@ -109,16 +113,24 @@ function VRSurfaceGui:UpdateEvents(Points)
         
         --Send the mouse button inputs.
         if InputPosition.Z >= TRIGGER_DOWN_THRESHOLD then
-            if not MouseDownFrames[Frame] then
-                MouseDownFrames[Frame] = true
-                if Frame:IsA("GuiButton") then
-                    Frame.MouseButton1Down:Fire(InputPosition.X,InputPosition.Y)
+            local InputId = FrameInputId[Frame]
+            if not self.LastInputs[InputId] or self.LastInputs[InputId].Z < TRIGGER_DOWN_THRESHOLD then
+                if not MouseDownFrames[Frame] then
+                    MouseDownFrames[Frame] = true
+                    if Frame:IsA("GuiButton") then
+                        Frame.MouseButton1Down:Fire(InputPosition.X,InputPosition.Y)
+                    end
+                    Frame.InputBegan:Fire({
+                        KeyCode = Enum.KeyCode.Unknown,
+                        UserInputType = Enum.UserInputType.MouseButton1,
+                        Position = InputPosition,
+                    })
                 end
-                Frame.InputBegan:Fire({
-                    KeyCode = Enum.KeyCode.Unknown,
-                    UserInputType = Enum.UserInputType.MouseButton1,
-                    Position = InputPosition,
-                })
+
+                --Capture the textbox.
+                if Frame:IsA("TextBox") and not Frame:IsFocused() then
+                    Frame:CaptureFocus()
+                end
             end
 
             --Move the ScrollingFrame.
@@ -133,11 +145,6 @@ function VRSurfaceGui:UpdateEvents(Points)
                     Frame.CanvasPosition = Vector2.new(math.clamp(Frame.CanvasPosition.X - DeltaInput.X,0,MaxCanvasPosition.X),math.clamp(Frame.CanvasPosition.Y - DeltaInput.Y,0,MaxCanvasPosition.Y))
                 end
                 ScrollLastPositions[Frame] = InputPosition
-            end
-
-            --Capture the textbox.
-            if Frame:IsA("TextBox") and not Frame:IsFocused() then
-                Frame:CaptureFocus()
             end
         elseif InputPosition.Z <= TRIGGER_UP_THRESHOLD and MouseDownFrames[Frame] then
             ScrollLastPositions[Frame] = nil
@@ -173,6 +180,9 @@ function VRSurfaceGui:UpdateEvents(Points)
         MouseDownFrames[Frame] = nil
         ScrollLastPositions[Frame] = nil
     end
+
+    --Store the last inputs.
+    self.LastInputs = Points
 end
 
 
