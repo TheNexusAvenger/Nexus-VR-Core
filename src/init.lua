af42e1e27d006f748a47156921c49ab13bc6238d
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2023-01-13 21:51:46 -0500

    Remove Nexus Project and Nexus Wrapped Instance.

--- src/init.lua
+++ src/init.lua
@@ -4,6 +4,22 @@ TheNexusAvenger
 Project for fetching resources of Nexus VR Core.
 --]]
 
-local NexusProject = require(script:WaitForChild("NexusProject"))
+local NexusProject = {}
 
-return NexusProject.new(script)
+
+
+--[[
+Returns a resource in Nexus VR Core.
+Legacy from Nexus Project.
+--]]
+function NexusProject:GetResource(Path: string): any
+    local Module = script
+    for _, PathPart in string.split(Path, ".") do
+        Module = (Module :: any)[PathPart]
+    end
+    return require(Module :: ModuleScript)
+end
+
+
+
+return NexusProject
